f86f5c0a910048c7d1990f1d01fc89376b551cb9
hi-roto <hirostudy2020@gmail.com>
2021-03-29 23:04:09 +0900

    エラー調査２

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -1,34 +1,34 @@
 class LinebotsController < ApplicationController
 require 'line/bot'
-Rails.logger.fatal 1111111
+Rails.logger.fatal 2222222
 protect_from_forgery except: [:callback]
-Rails.logger.fatal 1111111
+Rails.logger.fatal 3333333
 def callback
   body = request.body.read
   signature = request.env['HTTP_X_LINE_SIGNATURE']
-  Rails.logger.fatal 1111111
+  Rails.logger.fatal 4444444
   unless client.validate_signature(body, signature)
     return head :bad_request
-    Rails.logger.fatal 1111111
+    Rails.logger.fatal 555555
   end
   events = client.parse_events_from(body)
-  Rails.logger.fatal 1111111
+  Rails.logger.fatal 66666
   events.each do |event|
     case event
     when Line::Bot::Event::Message
-      Rails.logger.fatal 1111111
+      Rails.logger.fatal 777777
       case event.type
       when Line::Bot::Event::MessageType::Text
-        Rails.logger.fatal 1111111
+        Rails.logger.fatal 888888
         input = event.message['text']
         message = search_and_create_message(input)
         client.reply_message(event['replyToken'], message)
-        Rails.logger.fatal 1111111
+        Rails.logger.fatal 9999999
       end
     end
   end
   head :ok
-  Rails.logger.fatal 1111111
+  Rails.logger.fatal 121212
 end
 
   private 
@@ -37,7 +37,7 @@ end
     @client ||= Line::Bot::Client.new do |config|
       config.channel_secret = ENV['LINE_BOT_CHANNEL_SECRET']
       config.channel_token = ENV['LINE_BOT_CHANNEL_TOKEN']
-      Rails.logger.fatal 1111111
+      Rails.logger.fatal 131313
     end
   end
 
@@ -45,14 +45,14 @@ end
     RakutenWebService.configure do |c|
       c.application_id = ENV['RAKUTEN_APPID']
       c.affiliate_id = ENV['RAKUTEN_AFID']
-      Rails.logger.fatal 1111111
+      Rails.logger.fatal 14141414
     end
     res = RakutenWebService::Ichiba::Item.search(keyword: input, hits: 3, imageFlag: 1)
-    Rails.logger.fatal 1111111
+    Rails.logger.fatal 15151515
     items = []
     items = res.map{|item| item}
     make_reply_content(items)
-    Rails.logger.fatal 1111111
+    Rails.logger.fatal 16161616
   end
 
   def make_reply_content(items)
