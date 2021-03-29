b78ecf2010ef3a4212c8634277ca44b56ad9c023
hi-roto <hirostudy2020@gmail.com>
2021-03-29 21:59:02 +0900

    エラー調査

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -1,27 +1,34 @@
 class LinebotsController < ApplicationController
 require 'line/bot'
-
+Rails.logger.fatal 1111111
 protect_from_forgery except: [:callback]
-
+Rails.logger.fatal 1111111
 def callback
   body = request.body.read
   signature = request.env['HTTP_X_LINE_SIGNATURE']
+  Rails.logger.fatal 1111111
   unless client.validate_signature(body, signature)
     return head :bad_request
+    Rails.logger.fatal 1111111
   end
   events = client.parse_events_from(body)
+  Rails.logger.fatal 1111111
   events.each do |event|
     case event
     when Line::Bot::Event::Message
+      Rails.logger.fatal 1111111
       case event.type
       when Line::Bot::Event::MessageType::Text
+        Rails.logger.fatal 1111111
         input = event.message['text']
         message = search_and_create_message(input)
         client.reply_message(event['replyToken'], message)
+        Rails.logger.fatal 1111111
       end
     end
   end
   head :ok
+  Rails.logger.fatal 1111111
 end
 
   private 
@@ -30,6 +37,7 @@ end
     @client ||= Line::Bot::Client.new do |config|
       config.channel_secret = ENV['LINE_BOT_CHANNEL_SECRET']
       config.channel_token = ENV['LINE_BOT_CHANNEL_TOKEN']
+      Rails.logger.fatal 1111111
     end
   end
 
@@ -37,11 +45,14 @@ end
     RakutenWebService.configure do |c|
       c.application_id = ENV['RAKUTEN_APPID']
       c.affiliate_id = ENV['RAKUTEN_AFID']
+      Rails.logger.fatal 1111111
     end
     res = RakutenWebService::Ichiba::Item.search(keyword: input, hits: 3, imageFlag: 1)
+    Rails.logger.fatal 1111111
     items = []
     items = res.map{|item| item}
     make_reply_content(items)
+    Rails.logger.fatal 1111111
   end
 
   def make_reply_content(items)
